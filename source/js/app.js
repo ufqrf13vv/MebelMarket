71e363af2ba67a33a98fcc8a4ab8ec4a792571c2
Mikhail Ragulin <anarchy16@inbox.ru>
2018-03-26 16:53:30 +0600

    add adaptability

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,25 @@
 (function() {
-  'use strict';
+  	$(document).ready(function () {
+  		$('#filter, #menu').on('click', function(e) {
+			e.preventDefault();
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+			if ($(this).attr('id') == 'filter') {
+				menu($(this), '.filter__aside-wrapper');
+			} else {
+				menu($(this), '.main-menu__wrapper');
+			}
+  		});
+
+  		function menu(main, sibling) {
+  			var menu = main.siblings(sibling);
+
+  			if (menu.css('display') == 'none') {
+				menu.fadeIn();
+
+				return;
+			}
+			
+			menu.fadeOut();
+  		}
+  	});
 })();
